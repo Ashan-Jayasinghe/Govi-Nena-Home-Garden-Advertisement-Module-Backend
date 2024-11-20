32feed918e33317f373a1e72151ad0b6d6405707
Denuwan Sankalpa <143308121+Denuwan99@users.noreply.github.com>
2024-11-21 01:02:21 +0530

    added_update

--- db_connection.php
+++ db_connection.php
@@ -3,7 +3,7 @@ $host = '127.0.0.1'; // Since your MySQL is running on localhost
 $db = 'govinena'; // Your database name
 $user = 'root'; // MySQL user
 $pass = ''; // MySQL password
-$port = 3307; // The custom MySQL port you're using
+$port = 3306; // The custom MySQL port you're using
 
 // Create a new connection
 $conn = new mysqli($host, $user, $pass, $db, $port);
